2702ee8b47982cde2041ba0960aab11496e99098
Paul Bender <paulbender@live.com>
2021-06-26 15:26:45 -0400

    Bump to version v1.2

--- inc/plugins/limitcalendarevents.php
+++ inc/plugins/limitcalendarevents.php
@@ -29,7 +29,7 @@ function limitcalendarevents_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.1",
+		"version"			=> "1.2",
 		"codename"			=> "limitcalendarevents",
 		"compatibility"		=> "18*"
 	);
